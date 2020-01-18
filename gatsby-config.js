6a35ee725e552cb81be57d6b1a63d77d7d9f04e7
TERVER AOSU <terveraosu@gmail.com>
2020-01-18 14:31:45 +0100

    installed blog theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,36 @@
 module.exports = {
   siteMetadata: {
-    title: `Pandas Eating Lots`,
+    title: `TrueTech Information System`,
+    author: "Terver Aosu",
+    description: "A collection of my thoughts and writings.",
+    siteUrl: "https://amberley.blog/",
+    social: [
+      {
+        name: "twitter",
+        url: "https://twitter.com/amber1ey",
+      },
+      {
+        name: "github",
+        url: "https://github.com/amberleyromo",
+      },
+    ],
   },
   plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {
+        /*
+        - basePath defaults to `/`
+        - contentPath defaults to `content/posts`
+        - assetPath defaults to `content/assets`
+        - mdx defaults to `true`
+        */
+        basePath: `/blog`,
+        contentPath: `content/blogPosts`,
+        assetPath: `content/blogAssets`,
+        mdx: false,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
